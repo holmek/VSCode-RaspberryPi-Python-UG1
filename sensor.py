c65229128739c774f419502e402e348a7b5ad02a
Emil Holmgaard <124199961+holmek@users.noreply.github.com>
2024-02-05 19:43:22 +0100

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,33 +1,31 @@
-# Jeg importerer biblotekerne fra gpiozero/asyncio
-from gpiozero import MotionSensor
-from gpiozero import Buzzer
 import asyncio
+from gpiozero import Buzzer
+from gpiozero import MotionSensor
 
 # Dette er mine pins på rpi5, som er gpio 16 og 12
 sensor_pin = 16
 buzzer_pin = 12
 
 # Dette er mine objekter, som er pir, hvilket er motionsensoeren og buzzeren.
-pir = MotionSensor(sensor_pin, queue_len=1, sample_rate=10, threshold=5.0, partial=False, pin_factory=None) # 13.1.3. https://gpiozero.readthedocs.io/en/stable/api_input.html
+pir = MotionSensor(sensor_pin, queue_len=1, sample_rate=10, threshold=0.5, partial=False, pin_factory=None)
 buzzer = Buzzer(buzzer_pin)
 
 # Dette er min funktion som opfanger bevægelse og aktiverer buzzer, samt skriver en besked
 async def motion_sensor():
     while True:
         if pir.motion_detected:
-            print("Bevægelse opfanget i nærheden")
-            buzzer.beep(on_time=1, off_time=1, n=None, background=False) # 14.1.4. Buzzer https://gpiozero.readthedocs.io/en/stable/api_output.html
-            print("Buzzeren bliver aktiveret")
+            print("Bevægelse opfanget i nærheden. Buzzeren bliver aktiveret")
+            buzzer.beep(on_time=1, off_time=1, n=None, background=False)
         await asyncio.sleep(10)
 
 # Dette er min hovedfunktion hvor motion_detection bliver udløst
 async def main():
     try:
-        await motion_sensor()
+        task = asyncio.create_task(motion_sensor())
+        await asyncio.gather(task)
     except KeyboardInterrupt:
         print("Programmet lukker")
 
 # Dette gør at main kører når jeg starter script
 if __name__ == "__main__":
     asyncio.run(main())
-    
