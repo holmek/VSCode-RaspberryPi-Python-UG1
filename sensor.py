33c99fb1e74faa644bc5bf1d2b10207a33519234
Emil Holmgaard <124199961+holmek@users.noreply.github.com>
2024-02-05 19:35:43 +0100

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -8,15 +8,16 @@ sensor_pin = 16
 buzzer_pin = 12
 
 # Dette er mine objekter, som er pir, hvilket er motionsensoeren og buzzeren.
-pir = MotionSensor(sensor_pin, queue_len=1, sample_rate=10, threshold=0.5, partial=False, pin_factory=None) # 13.1.3. https://gpiozero.readthedocs.io/en/stable/api_input.html
+pir = MotionSensor(sensor_pin, queue_len=1, sample_rate=10, threshold=30, partial=False, pin_factory=None) # 13.1.3. https://gpiozero.readthedocs.io/en/stable/api_input.html
 buzzer = Buzzer(buzzer_pin)
 
 # Dette er min funktion som opfanger bevægelse og aktiverer buzzer, samt skriver en besked
 async def motion_sensor():
     while True:
         if pir.motion_detected:
-            print("Bevægelse opfanget i nærheden. Buzzeren bliver aktiveret")
+            print("Bevægelse opfanget i nærheden")
             buzzer.beep(on_time=1, off_time=1, n=None, background=False) # 14.1.4. Buzzer https://gpiozero.readthedocs.io/en/stable/api_output.html
+            print("Buzzeren bliver aktiveret")
         await asyncio.sleep(10)
 
 # Dette er min hovedfunktion hvor motion_detection bliver udløst
